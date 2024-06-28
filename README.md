55e59191db8587d900b96086e247b068ac276097
FabTrindade <fabrtrindae@gmail.com>
2024-06-28 18:31:38 -0300

    UseCase2 -> using sequential CallBacks

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## KotlinCoroutines&FlowUseCases
 ### A Android/Kotlin application focused in demonstrate uses cases of coroutines and flow 
 
+#####  (It is based on the initial project template from a Udemy course (Kotlin Coroutines and Flow for Android Development [2024]))
+
 ### Highlight features used:  
 _
--- app/src/main/java/com/fabscorp/coroutineusecasesonandroid/usecases/coroutines/usecase2/callbacks/SequentialNetworkRequestsCallbacksViewModel.kt
+++ app/src/main/java/com/fabscorp/coroutineusecasesonandroid/usecases/coroutines/usecase2/callbacks/SequentialNetworkRequestsCallbacksViewModel.kt
@@ -1,12 +1,57 @@
 package com.fabscorp.coroutineusecasesonandroid.usecases.coroutines.usecase2.callbacks
 
 import com.fabscorp.coroutineusecasesonandroid.base.BaseViewModel
+import com.fabscorp.coroutineusecasesonandroid.mock.AndroidVersion
+import com.fabscorp.coroutineusecasesonandroid.mock.VersionFeatures
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SequentialNetworkRequestsCallbacksViewModel(
     private val mockApi: CallbackMockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        val getAndroidVersion = mockApi.getRecentAndroidVersions()
+
+        getAndroidVersion.enqueue(object: Callback<List<AndroidVersion>> {
+            override fun onResponse(
+                call: Call<List<AndroidVersion>>,
+                response: Response<List<AndroidVersion>>
+            ) {
+                if (response.isSuccessful) {
+                    val mostRecentVersion = response.body()!!.last()
+                    val getAndroidFeatures = mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+
+                    getAndroidFeatures.enqueue(object: Callback<VersionFeatures>{
+                        override fun onResponse(
+                            call: Call<VersionFeatures>,
+                            response: Response<VersionFeatures>
+                        ) {
+                            if (response.isSuccessful) {
+                              uiState.value = UiState.Success(response.body()!!)
+                            } else {
+                                uiState.value = UiState.Error ("Network request failed!")
+                            }
+
+                        }
+
+                        override fun onFailure(call: Call<VersionFeatures>, t: Throwable) {
+                            uiState.value = UiState.Error ("Network request failed!")
+                        }
+
+                    })
+                } else {
+                    uiState.value = UiState.Error ("Network request failed!")
+                }
+
+            }
+
+            override fun onFailure(call: Call<List<AndroidVersion>>, t: Throwable) {
+                uiState.value = UiState.Error ("Something unexpected happening!")
+            }
+        })
     }
 }
