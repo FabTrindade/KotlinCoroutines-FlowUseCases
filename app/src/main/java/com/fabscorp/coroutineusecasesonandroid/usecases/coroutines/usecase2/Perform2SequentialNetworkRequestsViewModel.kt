ee7dcab274fb73f18ae4640dd3bd496bcfa952a8
FabTrindade <fabrtrindae@gmail.com>
2024-07-02 14:32:09 -0300

    UseCase2 -> with Coroutines Implementation

--- app/src/main/java/com/fabscorp/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/fabscorp/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,12 +1,26 @@
 package com.fabscorp.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.fabscorp.coroutineusecasesonandroid.base.BaseViewModel
 import com.fabscorp.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
+
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val mostRecentVersion = recentVersions.last()
+                val featuresMostRecentVersion = mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+                uiState.value = UiState.Success (featuresMostRecentVersion)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Network request failed!")
+            }
+        }
     }
 }
