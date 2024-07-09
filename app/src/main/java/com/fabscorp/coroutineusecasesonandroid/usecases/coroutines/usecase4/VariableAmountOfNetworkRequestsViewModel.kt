51cf0915e8630641dff2c31dd87d88dfac05afe2
FabTrindade <fabrtrindae@gmail.com>
2024-07-09 19:38:17 -0300

    UseCase4 -> in a sequential way

--- app/src/main/java/com/fabscorp/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
+++ app/src/main/java/com/fabscorp/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
@@ -1,14 +1,29 @@
 package com.fabscorp.coroutineusecasesonandroid.usecases.coroutines.usecase4
 
+import androidx.lifecycle.viewModelScope
 import com.fabscorp.coroutineusecasesonandroid.base.BaseViewModel
 import com.fabscorp.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class VariableAmountOfNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val androidVersions = mockApi.getRecentAndroidVersions()
+
+                val androidFeatures = androidVersions.map { androidVersion ->
+                    mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                }
+                uiState.value = UiState.Success(androidFeatures)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error ("Network request failed!")
+            }
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
