0e9dd8cbcd8d905e64c9258dfc2b62f9e7e9fef8
FabTrindade <fabrtrindae@gmail.com>
2024-07-13 21:19:29 -0300

    UseCase5 -> using withTimeout from kotlinx.coroutines

--- app/src/main/java/com/fabscorp/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
+++ app/src/main/java/com/fabscorp/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
@@ -1,14 +1,35 @@
 package com.fabscorp.coroutineusecasesonandroid.usecases.coroutines.usecase5
 
+import androidx.lifecycle.viewModelScope
 import com.fabscorp.coroutineusecasesonandroid.base.BaseViewModel
 import com.fabscorp.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import timber.log.Timber
 
 class NetworkRequestWithTimeoutViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest(timeout: Long) {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = withTimeout(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                uiState.value = UiState.Success(recentAndroidVersions)
+
+            } catch (timeOutException: TimeoutCancellationException) {
+                uiState.value = UiState.Error("Network request timed out!")
+            }
+            catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Network request failed!")
+            }
+        }
     }
 
 }
