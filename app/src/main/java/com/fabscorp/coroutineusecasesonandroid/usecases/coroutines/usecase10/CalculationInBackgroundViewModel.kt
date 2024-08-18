3d75d626938ac4db68371d752bfe60eb8b0efb19
FabTrindade <fabrtrindae@gmail.com>
2024-08-18 13:45:40 -0300

    UseCase10 -> Background Processing with Coroutines -> On Main Thread

--- app/src/main/java/com/fabscorp/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/com/fabscorp/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -1,10 +1,39 @@
 package com.fabscorp.coroutineusecasesonandroid.usecases.coroutines.usecase10
 
+import androidx.lifecycle.viewModelScope
 import com.fabscorp.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.launch
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
 
     fun performCalculation(factorialOf: Int) {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            var result: BigInteger = BigInteger.ONE
+
+            val computationDuration = measureTimeMillis {
+                result = calculateFactorialOf(factorialOf)
+            }
+
+            var resultString = ""
+
+            val stringConvertionDuration = measureTimeMillis {
+                resultString = result.toString()
+            }
+
+            uiState.value = UiState.Success (resultString, computationDuration, stringConvertionDuration)
+        }
+    }
+
+    private fun calculateFactorialOf (number: Int): BigInteger {
+        var factorial = BigInteger.ONE
+
+        for (i in 1 .. number) {
+            factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+        }
+        return factorial
     }
 }
