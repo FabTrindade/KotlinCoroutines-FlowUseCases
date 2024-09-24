d69c7a5001e798c496b4bf8eb4dcdffc8d9d37cd
FabTrindade <fabrtrindae@gmail.com>
2024-09-24 14:25:33 -0300

    Playground -> structuredconcorrency -> 2_job_hierarchy -> with child hierarchy

--- app/src/main/java/com/fabscorp/coroutineusecasesonandroid/playground/utils/structuredconcorrency/2_job_hierarchy.kt
+++ app/src/main/java/com/fabscorp/coroutineusecasesonandroid/playground/utils/structuredconcorrency/2_job_hierarchy.kt
@@ -10,10 +10,22 @@ fun main() {
     val scopeJob = Job()
     val scope = CoroutineScope(Dispatchers.Default + scopeJob)
 
-    var coroutineJob= scope.launch {
+    var childCoroutineJob: Job? = null
+    val coroutineJob= scope.launch {
+
+        childCoroutineJob = launch {
+            println("Starting child coroutine...")
+            delay(1000)
+        }
+
         println("Starting coroutine...")
         delay(1000)
     }
+
+    Thread.sleep(1000)
+
+    println("Is childCoroutineJob a child of coroutineJob? => ${coroutineJob.children.contains(childCoroutineJob)}")
+    //OUTPUT: Is childCoroutineJob a child of coroutineJob? => true
 
     println("Is coroutineJob a child of scopeJob? => ${scopeJob.children.contains(coroutineJob)}")
     //OUTPUT: Is coroutineJob a child of scopeJob? => true
