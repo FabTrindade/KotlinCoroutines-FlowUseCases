c5e0357a2b9e96460f8c76c4b53715b06e62c45c
FabTrindade <fabrtrindae@gmail.com>
2024-10-22 22:56:31 -0300

    Playground -> structuredconcorrency -> 4_cancelling_parents_and_children

---
+++ app/src/main/java/com/fabscorp/coroutineusecasesonandroid/playground/utils/structuredconcorrency/4_cancelling_parents_and_children.kt
@@ -0,0 +1,39 @@
+package com.fabscorp.coroutineusecasesonandroid.playground.utils.structuredconcorrency
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit>{
+    val scope = CoroutineScope(Dispatchers.Default)
+
+    scope.launch {
+        delay(1000)
+        println("Coroutine 1 completed!")
+    }.invokeOnCompletion { throwable ->
+        if (throwable is CancellationException){
+            println("Coroutine 1 was cancelled!")
+        }
+    }
+    scope.launch {
+        delay(1000)
+        println("Coroutine 2 completed!")
+    }.invokeOnCompletion { throwable ->
+        if (throwable is CancellationException){
+            println("Coroutine 2 was cancelled!")
+        }
+    }
+
+    scope.cancel();
+    scope.coroutineContext[Job]!!.join()
+    //OUTPUT:
+    //Coroutine 2 was cancelled!
+    //Coroutine 1 was cancelled!
+    //Process finished with exit code 0
+
+}
